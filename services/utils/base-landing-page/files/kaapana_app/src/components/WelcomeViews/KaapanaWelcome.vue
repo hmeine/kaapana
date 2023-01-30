0d58ee17a9243b26ce0cfdbac2ccca83ca562135
Lorenz Feineis <lorenz.feineis@dkfz-heidelberg.de>
2023-01-30 15:16:10 +0100

    Change commonData for landing-page and use more high resolution logo of AIH Cluster.

--- services/utils/base-landing-page/files/kaapana_app/src/components/WelcomeViews/KaapanaWelcome.vue
+++ services/utils/base-landing-page/files/kaapana_app/src/components/WelcomeViews/KaapanaWelcome.vue
@@ -2,12 +2,12 @@
 <template lang="pug">
   v-layout(row='', wrap='')
     v-flex.text-xs-left(sm8, d-flex)
-      v-card.kaapana-opacity-card
+      v-card.kaapana-opacity-card(width='160%')
         v-card-text.text-xs-left
           v-layout(row='', wrap='')
             v-flex(xs12, py-1)
               div(style={'margin': '9px', 'margin-top': '10px'})
-                img(src='../../assets/img/aihealth.png', alt='Vuetify', width='75%')
+                img(src='../../assets/img/aihealth.png', alt='Vuetify', width='100%')
     v-flex.text-xs-left(sm4 d-flex)
       v-card.kaapana-opacity-card(width='100%')
         v-card-text.text-xs-left
@@ -15,7 +15,7 @@
           hr.my-2
           v-layout(row='', wrap='')
             v-flex(xs12, py-1)
-              v-icon mdi-email
+              v-icon mdi-slack
               |&nbsp;
               a(href="https://aihcluster.slack.com/archives/C03P03WGJSJ") cc-software
 </template>
